7c36fb376a2baf41a40fd5673769d838b3602505
Thanasis Davvetas <davve.ath@gmail.com>
2017-06-07 12:23:35 +0300

    pilot update

--- classification_pilot/sc5_sextant/sc5.sh
+++ classification_pilot/sc5_sextant/sc5.sh
@@ -9,5 +9,4 @@ rm -rf /tomcat/webapps/Sextant_v2.0/index.html
 cp -rp /bde-pilot-2/classification_pilot/Sextant_v2.0/assets/ /tomcat/webapps/Sextant_v2.0/
 cp /bde-pilot-2/classification_pilot/Sextant_v2.0/index.html /tomcat/webapps/Sextant_v2.0/
 cp -rp /bde-pilot-2/classification_pilot/Sextant_v2.0/data/ /tomcat/webapps/Sextant_v2.0/
-pip install -r /bde-pilot-2/classification_pilot/requirements.txt
-pip install --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
+pip install -r /bde-pilot-2/classification_pilot/requirements.txt && pip install --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
