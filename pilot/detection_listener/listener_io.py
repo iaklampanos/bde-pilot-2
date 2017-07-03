daeb0bc9026533d0d6bd82ac6ff651f4e01f7bca
Thanasis Davvetas <davve.ath@gmail.com>
2017-07-03 16:57:17 +0300

    ref

--- pilot/detection_listener/listener_io.py
+++ pilot/detection_listener/listener_io.py
@@ -34,12 +34,10 @@ import base64
 
 from celery import Celery
 
-app = Flask(__name__)
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 
 inp = None
@@ -558,4 +556,5 @@ for row in cur:
     os.system('rm ' + APPS_ROOT + '/' + str(os.getpid())+row[1])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    # app.run(host='0.0.0.0',debug=True)
+    socketio.run(app)
