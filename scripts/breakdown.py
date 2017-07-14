12235e711b2f9d681925e149b996beae8cf4668e
Thanasis Davvetas <davve.ath@gmail.com>
2017-07-14 14:59:43 +0300

    Backend update

--- scripts/breakdown.py
+++ scripts/breakdown.py
@@ -1,3 +1,9 @@
+import sys
+
+BDE_PILOT_SRC = '..'
+
+sys.path.append(BDE_PILOT_SRC)
+
 from netcdf_subset import netCDF_subset
 from netCDF4 import Dataset, num2date, date2num
 from operator import attrgetter
@@ -6,30 +12,25 @@ from argparse import ArgumentParser
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Extract variables from netcdf file')
-    parser.add_argument('-im', '--input_mod', required=True, type=str,
-                        help='modified file')
-    parser.add_argument('-ir', '--input_real', required=True, type=str,
-                        help='real file')
+    parser.add_argument('-i', '--input', required=True, type=str,
+                        help='netcdf file')
+    parser.add_argument('-o', '--output', required=True, type=str,
+                        help='netcdf file')
     opts = parser.parse_args()
-    getter = attrgetter('input_mod','input_real')
-    inp,inpr = getter(opts)
-    dsin = Dataset(inp,'r')
-    dsinr = Dataset(inpr,'r')
-    nc_mod = netCDF_subset(dsin, levels=[500,700,900],sub_vars=['UU','VV','TT'],
-                 lvlname='num_metgrid_levels', timename='Times',
-                 time_unit='hours since 1900-01-01 00:00:00',
-                 time_cal='gregorian', ncar_lvls=None)
-    nc = netCDF_subset(dsinr, levels=[500,700,900],sub_vars=['UU','VV','TT'],
+    getter = attrgetter('input','output')
+    inp,outp = getter(opts)
+    if '/' not in outp:
+        outp = outp + '/'
+    nc_mod = netCDF_subset(inp, levels=[500,700,900],sub_vars=['UU','VV','TT'],
                  lvlname='num_metgrid_levels', timename='Times',
                  time_unit='hours since 1900-01-01 00:00:00',
                  time_cal='gregorian', ncar_lvls=None)
     dates = nc_mod.get_times()
     ltimes = len(nc_mod._dataset[nc_mod._time_name][:])
     times = range(ltimes)
-    tidx = times[0::13]
+    tidx = times[0::12]
     for pos,i in enumerate(tidx):
         try:
-            nc.exact_copy_file('./nc/'+dates[i].strftime("%Y-%m-%d_%H:%M:%S")+'.nc'
-                               ,range(tidx[pos],tidx[pos+1]))
+            nc_mod.exact_copy_file(outp+str(pos)+'.nc',range(tidx[pos],tidx[pos+1]))
         except IndexError:
             continue
