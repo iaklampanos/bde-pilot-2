060f69aa89b17e94165613caf241ab24a4a3794c
Thanasis Davvetas <davve.ath@gmail.com>
2017-07-14 17:20:59 +0300

    Backend update

---
+++ pilot/detection_listener/test/dbconn.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+
+
+class DBConn:
+
+    instance = None
+
+    def __init__(self):
+        with open('db_info.json', 'r') as data_file:
+            dbpar = json.load(data_file)
+        engine = create_engine('postgresql+psycopg2://' + dbpar['user'] + ':' +
+                 base64.b64decode(dbpar['pass']) + '@' + dbpar['host'] + '/'
+                 + dbpar['dbname'] + '')
+        engine.connect()
+        instance = engine
+
+    def __new__(cls):
+        if DBConn.instance is None:
+            DBConn.instance = object.__new__(cls)
+        return DBConn.instance
